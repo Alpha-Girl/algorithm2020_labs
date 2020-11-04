39a091c204fec415c265dda31ea69cc1bc96218f
Alpha-Girl <alphaboy@mail.ustc.edu.cn>
2020-11-04 20:53:56 +0800

    save(sort.c)

--- lab1/src/sort.c
+++ lab1/src/sort.c
@@ -2,90 +2,62 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#include "sort.h"
 
-void InsertionSort(int *a, int n);
-void QuickSort(int *a, int n);
-void HeapSort(int *a, int n);
-void MergeSort(int *a, int start, int end);
-void CountSort(int *a, int n);
+int CopyAndCheck(int *a, int *b, int n);
 
 int main()
 {
-    int n, *array, size, *save;
-    printf("Please input n:\n");
+    int n, *array, size, *save, *result, k,*t;
+    int start,end;
+    //print README
+    printf("***README***\n");
+    printf("————————————————————————————————————————————————————————————————\n");
+    printf("The sort.exe will 'k' arraies, of which size are 'n' of 2,\n");
+    printf("using InsertionSort,QuickSort,HeapSort,MergeSort,andCountingSort.\n");
+    printf("Then it will output the average time of each algorithm.\n");
+    printf("————————————————————————————————————————————————————————————————\n");
+    //get parameter
+    printf("Now plz input k:\n");
+    scanf("%d", &k);
+    printf("Now plz input n:\n");
     scanf("%d", &n);
+    printf("————————————————————————————————————————————————————————————————\n");
+    printf("Starting...\n");
+
+    //init
     size = pow(2, n);
     array = (int *)calloc(size, sizeof(int));
     save = (int *)calloc(size, sizeof(int));
+    result = (int *)calloc(size, sizeof(int));
+    t=(int *)calloc(k*5,sizeof(int));
     srand(time(NULL));
     for (int i = 0; i < size; i++)
     {
         array[i] = rand();
         save[i] = array[i];
     }
-    printf("Starting...\n");
     //Insertion Sort
+    InsertionSort(array, size);
+
+    printf("Ending...\n");
     free(array);
     free(save);
+    free(result);
+    free(t);
     return 0;
 }
 
-//Insertion Sort
-void InsertionSort(int *a, int n)
+//copy
+int CopyAndCheck(int *a, int *b, int n)
 {
-    for (int j = 1; j < n; j++)
+    int flag = 1;
+    for (int i = 0; i < n - 1; i++)
     {
-        int key = a[j];
-        int i = j - 1;
-        while (i >= 0 && a[i] > key)
-        {
-            a[i + 1] = a[i];
-            i = i - 1;
-        }
-        a[i + 1] = key;
+        if (a[i] > a[i + 1])
+            flag = 0;
+        a[i] = b[i];
     }
-}
-
-//Merge Sort
-void Merge(int *a, int start, int q, int end)
-{
-    int m = q - start + 1;
-    int n = end - q;
-    int *L, *R, i, j;
-    L = (int *)calloc(m + 1, sizeof(int));
-    R = (int *)calloc(n + 1, sizeof(int));
-    for (i = 0; i < m; i++)
-        L[i] = a[start + i - 1];
-    for (j = 0; j < n; j++)
-        R[j] = a[q + j];
-    L[m] = INT_MAX;
-    R[n] = INT_MAX;
-    i = 0;
-    j = 0;
-    for (int k = start; k <= end; k++)
-    {
-        if (L[i] <= R[j])
-        {
-            a[k] = L[i];
-            i++;
-        }
-        else
-        {
-            a[k] = R[j];
-            j++;
-        }
-    }
-    free(L);
-    free(R);
-}
-
-void MergeSort(int *a, int start, int end)
-{
-    if (start < end)
-    {
-        int q = (start + end) / 2;
-        MergeSort(a, start, q);
-        MergeSort(a, q + 1, end);
-        Merge(a, start, q, end);
-    }
+    a[n - 1] = b[n - 1];
+    return flag;
 }
