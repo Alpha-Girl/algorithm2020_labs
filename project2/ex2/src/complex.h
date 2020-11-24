8f22cf54b0127efdedf85f8118602cdaca2bae82
Alpha-Girl <alphaboy@mail.ustc.edu.cn>
2020-11-24 17:45:45 +0800

    complex.h

---
+++ project2/ex2/src/complex.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+typedef struct complex{
+    float Re;
+    float Im;
+}complex;
+
+void plus(complex* a,complex* b,complex *result){
+    result->Re=a->Re+b->Re;
+    result->Im=a->Im+b->Im;
+}
+
+void miuns(complex* a,complex* b,complex *result){
+    result->Re=a->Re-b->Re;
+    result->Im=a->Im-b->Im;
+}
+
+void multiply(complex* a,complex* b,complex *result){
+    result->Re=a->Re*b->Re-a->Im*b->Im;
+    result->Im=a->Re*b->Im+a->Im*b->Re;
+}
+
+void exp_i(float a,complex *result){
+    result->Re=cos(a*M_PI);
+    result->Im=sin(a*M_PI);
+}
