0328d9af96a4200ce0e46395d0362e731986d5cc
Alpha-Girl <alphaboy@mail.ustc.edu.cn>
2020-11-24 17:54:38 +0800

    code style

--- project2/ex2/src/complex.h
+++ project2/ex2/src/complex.h
@@ -2,27 +2,32 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
-typedef struct complex{
+typedef struct complex
+{
     float Re;
     float Im;
-}complex;
+} complex;
 
-void plus(complex* a,complex* b,complex *result){
-    result->Re=a->Re+b->Re;
-    result->Im=a->Im+b->Im;
+void plus(complex *a, complex *b, complex *result)
+{
+    result->Re = a->Re + b->Re;
+    result->Im = a->Im + b->Im;
 }
 
-void miuns(complex* a,complex* b,complex *result){
-    result->Re=a->Re-b->Re;
-    result->Im=a->Im-b->Im;
+void miuns(complex *a, complex *b, complex *result)
+{
+    result->Re = a->Re - b->Re;
+    result->Im = a->Im - b->Im;
 }
 
-void multiply(complex* a,complex* b,complex *result){
-    result->Re=a->Re*b->Re-a->Im*b->Im;
-    result->Im=a->Re*b->Im+a->Im*b->Re;
+void multiply(complex *a, complex *b, complex *result)
+{
+    result->Re = a->Re * b->Re - a->Im * b->Im;
+    result->Im = a->Re * b->Im + a->Im * b->Re;
 }
 
-void exp_i(float a,complex *result){
-    result->Re=cos(a*M_PI);
-    result->Im=sin(a*M_PI);
+void exp_i(float a, complex *result)
+{
+    result->Re = cos(a * M_PI);
+    result->Im = sin(a * M_PI);
 }
