c54dafca0cd7d03f12d7119e742f06967b43de73
Alpha-Girl <alphaboy@mail.ustc.edu.cn>
2020-12-07 11:52:50 +0800

    ok todo:style

--- project2/ex1/src/martix_chain.c
+++ project2/ex1/src/martix_chain.c
@@ -4,40 +4,50 @@
 #define MAX_LENGTH 100
 void martix_chain_order(int *array, int length);
 void print_optimal_parens(int i, int j);
-int ReadData(FILE *f);
+int ReadData();
 int input[MAX_LENGTH];
 long long m[MAX_LENGTH][MAX_LENGTH];
 int s[MAX_LENGTH][MAX_LENGTH];
+FILE *in;
 int main()
 {
-    FILE *in, *out;
+    FILE *out;
     int leng;
     printf("This is the program to solve martix chain order.\n");
     in = fopen("../input/2_1_input.txt", "r");
-    while (in!=EOF)
+    while (in != EOF)
     {
-        leng=ReadData(in);
-        martix_chain_order(input,leng);
-        print_optimal_parens(1,leng);
+        leng = ReadData(in);
+        martix_chain_order(input, leng);
+        printf("\n");
+        /*for (int i = 1; i <= leng; i++)
+        {
+            for (int j = 1; j <= leng; j++)
+            {
+                printf("%d ", s[i][j]);
+            }
+            printf("\n");
+        }*/
+        print_optimal_parens(1, leng);
     }
-    
 }
 
-int ReadData(FILE *f)
+int ReadData()
 {
     int n;
     int i;
-    fscanf(f,"%d\n",&n);
-    for(i=0;i<n-1;i++){
-        fscanf(f,"%d",&input[i]);
-    }
-    fscanf(f,"%d\n",&input[i]);
+    fscanf(in, "%d\n", &n);
+    for (i = 0; i < n; i++)
+        fscanf(in, "%d", &input[i]);
+    fscanf(in, "%d\n", &input[i]);
+    for (i = 0; i <= n; i++)
+        printf("%d ", input[i]);
     return n;
 }
 
 void martix_chain_order(int *array, int length)
 {
-    int n = length - 1;
+    int n = length;
 
     int i, j, k, l;
     long long tmp;
@@ -71,8 +81,8 @@ void print_optimal_parens(int i, int j)
     else
     {
         printf("(");
-        print_optimal_parens(i, s[i, j]);
-        print_optimal_parens(s[i, j] + 1, j);
+        print_optimal_parens(i, s[i][j]);
+        print_optimal_parens(s[i][j] + 1, j);
         printf(")");
     }
 }
